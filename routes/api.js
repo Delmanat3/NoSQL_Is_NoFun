c271451e82df4733d249edc16de1a32517650cf4
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-12 14:18:20 -0600

    fuckywuckygoestheducky

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,66 @@
-const router = require("express").Router()
-// getLastWorkout()
-//get route returns all workouts -1 on front for last
-///api/workouts
+const router = require("express").Router();
+const mongoose = require('mongoose');
+const db = require('../models')
+const path = require('path');
 
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate( [
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" } 
+      }
+    },
+  ] )
+    .sort({ day: 1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+// router.get('/api/workouts/',async(req,res)=>{
+// const dbData=await db.Workout.findOne({})
+// })
+router.put("/api/workouts/:id", ( req, res) => {
+    // Find a workout document by id and add the new exercise to the array of exercises
+    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
+      .then(dbWorkout => {
+
+        res.json(dbWorkout);
+
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 //addExercise()
 //put route 
-///apit/workouts/:id
-
+///api/workouts/:id
+router.post('/api/workouts',({body},res)=>{
+    db.Workout.create(body).then(
+        data=>{res.json(data)}
+    )
+})
 //createWorkout()
 //posst
 ///api/workouts
 
 // getWorkoutsInRange()
 ///api/workouts/range
+router.get('/api/workouts/range',(req,res)=>{
+    db.Workout.aggregate([
+        {
+          $addFields: { totalDuration: { $sum: "$exercises.duration" } }
+        },
+      ])
+      .sort({ day: 1 })
+      .then(workouts => {
+        const lastSevenWorkouts = workouts.slice(0,7)
+        res.json(lastSevenWorkouts);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      })
+    });
 module.exports=router
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000
 
 const app = express();
 //app.use(express)
-//app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,6 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
