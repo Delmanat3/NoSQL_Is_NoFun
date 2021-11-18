b22445b983164976f54f5f5990a1ebc1295ccdf2
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-18 15:10:40 -0600

    fuck a model

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,31 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const workoutSchema=new Schema({
-    type:{
-        type:String,
-        trim:true,
-       // required:"enter workout type"
-    },
-    name:{
-        type:String,
-        trim:true,
-        // required:"this is the mane",
-    },
-    duration:{
-        type:Number,
-    },
-    weight:Number,
-    reps:Number,
-    sets:Number
+   day:{
+       type:Date,
+        default:Date.now(),
+   },
+    exercises:[{
+        
+            type:{
+                type:String,
+                trim:true,
+               // required:"enter workout type"
+            },
+            name:{
+                type:String,
+                trim:true,
+                // required:"this is the mane",
+            },
+            duration:{
+                type:Number,
+            },
+        weight:Number,
+        reps:Number,
+        sets:Number,
+        distance:Number,
+}]
+  
 })
 const Workout = mongoose.model("Workout", workoutSchema);
 
